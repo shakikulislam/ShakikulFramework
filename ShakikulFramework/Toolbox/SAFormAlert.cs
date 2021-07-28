e555b2e37fbb9d34754b9952e9ceb08c4e09f288
unknown <shakikulislam625010@gmail.com>
2021-07-28 13:00:36 +0600

    Add SAFormAlert

---
+++ ShakikulFramework/Toolbox/SAFormAlert.cs
@@ -0,0 +1,133 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using ShakikulFramework.Properties;
+
+namespace ShakikulFramework
+{
+    public partial class FormAlert : Form
+    {
+        public FormAlert()
+        {
+            InitializeComponent();
+        }
+
+        private enum ActionEnum
+        {
+            Wait, Start, Close
+        }
+
+        public enum TypeEnum
+        {
+            Success, Warning, Error, Information, Delete
+        }
+
+        private ActionEnum _action;
+        private int _x, _y;
+        
+        private void pictureBoxClose_Click(object sender, System.EventArgs e)
+        {
+            timerAlert.Interval = 1;
+            _action = ActionEnum.Close;
+        }
+
+        private void timerAlert_Tick(object sender, System.EventArgs e)
+        {
+            switch (_action)
+            {
+                case ActionEnum.Wait:
+                    timerAlert.Interval = 5000;
+                    _action = ActionEnum.Close;
+                    break;
+                case ActionEnum.Start:
+                    timerAlert.Interval = 1;
+                    Opacity += 0.1;
+                    if (_x<this.Location.X)
+                    {
+                        this.Left--;
+                    }
+                    else
+                    {
+                        if (this.Opacity==1.0)
+                        {
+                            _action = ActionEnum.Wait;
+                        }
+                    }
+                    break;
+                case ActionEnum.Close:
+                    timerAlert.Interval = 1;
+                    this.Opacity -= 0.1;
+                    this.Left -= 3;
+                    if (this.Opacity==0.0)
+                    {
+                        this.Close();
+                    }
+                    break;             
+            }
+        }
+        
+        private void Alert(string message, TypeEnum type)
+        {
+            this.Opacity = 0.0;
+            this.StartPosition = FormStartPosition.Manual;
+
+            string formName;
+
+            for (int i = 1; i < Screen.PrimaryScreen.WorkingArea.Height / this.Height; i++)
+            {
+                formName = "Alert" + i;
+                FormAlert formAlert = (FormAlert)Application.OpenForms[formName];
+
+                if (formAlert == null)
+                {
+                    this.Name = formName;
+                    _x = (Screen.PrimaryScreen.WorkingArea.Width - this.Width) + 15;
+                    _y = (Screen.PrimaryScreen.WorkingArea.Height - (this.Height * i)) - (5 * i);
+                    this.Location = new Point(_x, _y);
+                    break;
+                }
+            }
+            
+            //_x = Screen.PrimaryScreen.WorkingArea.Width - this.Width - 5;
+            labelMessage.Text = message;
+
+            switch (type)
+            {
+                case TypeEnum.Success:
+                    pictureBoxIcon.Image = Resources.Success;
+                    BackColor = Color.SeaGreen;
+                    break;
+                case TypeEnum.Error:
+                    pictureBoxIcon.Image = Resources.Error;
+                    BackColor = Color.DarkRed;
+                    break;
+                case TypeEnum.Information:
+                    pictureBoxIcon.Image = Resources.Info;
+                    BackColor = Color.RoyalBlue;
+                    break;
+                case TypeEnum.Warning:
+                    pictureBoxIcon.Image = Resources.Warning;
+                    BackColor = Color.DarkOrange;
+                    break;
+                case TypeEnum.Delete:
+                    pictureBoxIcon.Image = Resources.Delete;
+                    BackColor = Color.Red;
+                    break;
+            }
+
+            TopMost = true;
+            Show();
+
+            _action = ActionEnum.Start;
+
+            timerAlert.Interval = 1;
+            timerAlert.Start();
+        }
+
+        public void ShowAlert(string message, TypeEnum type)
+        {
+            var alert=new FormAlert();
+            alert.Alert(message, type);
+        }
+
+    }
+}
---
+++ ShakikulFramework/Toolbox/SAFormAlert.designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ShakikulFramework
+{
+    partial class FormAlert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
+            this.timerAlert = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.BackColor = System.Drawing.Color.Transparent;
+            this.labelMessage.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.ForeColor = System.Drawing.Color.White;
+            this.labelMessage.Location = new System.Drawing.Point(46, 8);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(202, 35);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "দয়া করে এই ফর্মটি পূরণ করুন এবং আবার চেষ্টা করুন।";
+            this.labelMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelMessage.UseCompatibleTextRendering = true;
+            // 
+            // pictureBoxClose
+            // 
+            this.pictureBoxClose.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxClose.Image = global::ShakikulFramework.Properties.Resources.Cancel;
+            this.pictureBoxClose.Location = new System.Drawing.Point(254, 13);
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.Size = new System.Drawing.Size(25, 25);
+            this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxClose.TabIndex = 1;
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            // 
+            // pictureBoxIcon
+            // 
+            this.pictureBoxIcon.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxIcon.Image = global::ShakikulFramework.Properties.Resources.Info;
+            this.pictureBoxIcon.Location = new System.Drawing.Point(5, 8);
+            this.pictureBoxIcon.Name = "pictureBoxIcon";
+            this.pictureBoxIcon.Size = new System.Drawing.Size(35, 35);
+            this.pictureBoxIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIcon.TabIndex = 1;
+            this.pictureBoxIcon.TabStop = false;
+            // 
+            // timerAlert
+            // 
+            this.timerAlert.Tick += new System.EventHandler(this.timerAlert_Tick);
+            // 
+            // FormAlert
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(296, 50);
+            this.Controls.Add(this.pictureBoxIcon);
+            this.Controls.Add(this.pictureBoxClose);
+            this.Controls.Add(this.labelMessage);
+            this.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAlert";
+            this.Text = "Form_Alert";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxClose;
+        private System.Windows.Forms.PictureBox pictureBoxIcon;
+        internal System.Windows.Forms.Timer timerAlert;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
