3f11ea889d19b6dac136ab35d5b349530ceb5b3e
Shakikul Islam <shakikulislam625010@gmail.com>
2021-11-06 04:46:27 +0600

    Add CumstoTextBox TextBoxBorder BorderRadius Placeholder

---
+++ ShakikulFramework/Toolbox/SATextBox.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace ShakikulFramework.Toolbox
+{
+    [DefaultEvent("TextChanged")]
+    public partial class SATextBox : UserControl
+    {
+        //Fields
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlineStyle = false;
+        private Color borderFocusColor = Color.HotPink;
+        private bool isFocused = false;
+        private int borderRadius = 0;
+        private Color placeholderColor = Color.DarkGray;
+        private string placeholderText = "";
+        private bool isPlaceholder = false;
+        private bool isPasswordChar = false;
+
+        //Constructor
+        public SATextBox()
+        {
+            InitializeComponent();
+        }
+
+        //Events
+        public event EventHandler TextChanged;
+
+        #region Properties
+        
+        [Category("Shakikul Framework")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public bool UnderlineStyle
+        {
+            get { return underlineStyle; }
+            set
+            {
+                underlineStyle = value;
+                this.Invalidate();
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public bool PasswordChar
+        {
+            get { return isPasswordChar; }
+            set
+            {
+                isPasswordChar = value;
+                textBox1.UseSystemPasswordChar = value;
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public bool Multiline
+        {
+            get { return textBox1.Multiline; }
+            set { textBox1.Multiline = value; }
+        }
+
+        [Category("Shakikul Framework")]
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set
+            {
+                base.BackColor = value;
+                textBox1.BackColor = value;
+            }
+
+        }
+
+        [Category("Shakikul Framework")]
+        public override Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set
+            {
+                base.ForeColor = value;
+                textBox1.ForeColor = value;
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public override Font Font
+        {
+            get { return base.Font; }
+            set
+            {
+                base.Font = value;
+                textBox1.Font = value;
+                if (this.DesignMode)
+                {
+                    UpdateControlHeight();
+                }
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public string Texts
+        {
+            get { return isPlaceholder ? "" : textBox1.Text; }
+            set
+            {
+                textBox1.Text = value;
+                SetPlaceholder();
+            }
+        }
+        
+        [Category("Shakikul Framework")]
+        public Color BorderFocusColor
+        {
+            get { return borderFocusColor; }
+            set { borderFocusColor = value; }
+        }
+
+        [Category("Shakikul Framework")]
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set {
+                if (value>=0)
+                {
+                    borderRadius = value;
+                    this.Invalidate(); //Redraw control
+                }
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public Color PlaceholderColor
+        {
+            get { return placeholderColor; }
+            set
+            {
+                placeholderColor = value;
+                if (isPasswordChar)
+                {
+                    textBox1.ForeColor = value;
+                }
+            }
+        }
+
+        [Category("Shakikul Framework")]
+        public string PlaceholderText
+        {
+            get { return placeholderText; }
+            set
+            {
+                placeholderText = value;
+                textBox1.Text = "";
+                SetPlaceholder();
+            }
+        }
+
+        private void SetPlaceholder()
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)&&placeholderText!="")
+            {
+                isPlaceholder = true;
+                textBox1.Text = placeholderText;
+                textBox1.ForeColor = placeholderColor;
+                if (isPasswordChar)
+                {
+                    textBox1.UseSystemPasswordChar = false;
+                }
+            }
+        }
+        private void RemovePlaceholder()
+        {
+            if (isPlaceholder && placeholderText!="")
+            {
+                isPlaceholder = false;
+                textBox1.Text = "";
+                textBox1.ForeColor = this.ForeColor;
+                if (isPasswordChar)
+                {
+                    textBox1.UseSystemPasswordChar = true;
+                }
+            }
+        }
+
+        #endregion
+
+        //Overridden method
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            if (borderRadius > 0) //Rounded TextBox
+            {
+                //Fields
+                var rectBorderSmooth = this.ClientRectangle;
+                var rectBorder = Rectangle.Inflate(rectBorderSmooth, -borderSize, -borderSize);
+                int smootSize = borderSize > 0 ? borderSize : 0;
+
+                using (GraphicsPath pathBorderSmooth = GetFigurePath(rectBorderSmooth, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penBorderSmooth = new Pen(this.Parent.BackColor, smootSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    //Drawing
+                    this.Region = new Region(pathBorderSmooth); //Set the rounded region of UserControl
+                    if (borderRadius > 15) SetTextBoxRoundedRegion(); //Set the rounded region of TextBox component
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    penBorder.Alignment = PenAlignment.Center;
+                    if (isFocused) penBorder.Color = borderFocusColor;
+
+                    if (underlineStyle) //Line style
+                    {
+                        //Draw border smoothing
+                        graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                        //Draw border
+                        graph.SmoothingMode = SmoothingMode.None;
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    }
+                    else //Normal style
+                    {
+                        //Draw border smoothing
+                        graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                        //Draw border
+                        graph.DrawPath(penBorder, pathBorder);
+
+                    }
+
+                }
+            }
+            else //Normal TextBox
+            {
+                //Draw border
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    this.Region = new Region(this.ClientRectangle);
+                    penBorder.Alignment = PenAlignment.Inset;
+
+                    if (isFocused) penBorder.Color = borderFocusColor;
+
+                    if (underlineStyle) //Line style
+                    {
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    }
+                    else //Normal style
+                    {
+                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void SetTextBoxRoundedRegion()
+        {
+            GraphicsPath pathTxt;
+            if (Multiline)
+            {
+                pathTxt = GetFigurePath(textBox1.ClientRectangle, borderRadius - borderSize);
+                textBox1.Region = new Region(pathTxt);
+            }
+            else
+            {
+                pathTxt = GetFigurePath(textBox1.ClientRectangle, borderSize * 2);
+                textBox1.Region = new Region(pathTxt);
+            }
+        }
+
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path=new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.DesignMode)
+            {
+                UpdateControlHeight();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        #region MyRegion
+
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline==false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize=new Size(0,txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+        #endregion Private method
+
+        #region TextBox events
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //this.OnTextChanged(e);
+        }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
+        }
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.OnMouseEnter(e);
+        }
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.OnMouseLeave(e);
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            isFocused = true;
+            this.Invalidate();
+            RemovePlaceholder();
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            isFocused = false;
+            this.Invalidate();
+            SetPlaceholder();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.OnKeyPress(e);
+        }
+
+        #endregion
+
+    }
+}
