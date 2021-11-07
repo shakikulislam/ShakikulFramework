ab75e92d77c98cd5504270227ba5d5ba9d059487
Shakikul Islam <shakikulislam625010@gmail.com>
2021-11-07 11:58:44 +0600

    Solve Decrypt problem

--- ShakikulFramework/Method/Password.cs
+++ ShakikulFramework/Method/Password.cs
@@ -9,7 +9,6 @@ namespace ShakikulFramework.Method
         private string hashes = "$@A%l$o*@";
         public string Encrypt(string password)
         {
-            byte[] result;
             byte[] data = UTF8Encoding.UTF8.GetBytes(password);
             using (MD5CryptoServiceProvider md5=new MD5CryptoServiceProvider())
             {
@@ -17,33 +16,33 @@ namespace ShakikulFramework.Method
                 using (TripleDESCryptoServiceProvider tripleDes=new TripleDESCryptoServiceProvider(){Key = keys,Mode = CipherMode.ECB,Padding = PaddingMode.PKCS7})
                 {
                     ICryptoTransform cryptoTransform = tripleDes.CreateEncryptor();
-                    result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    byte[] result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(result, 0, result.Length);
                 }
             }
 
-            return Convert.ToBase64String(result, 0, result.Length);
+            return "";
         }
         
         public string Decrypt(string encryptPassword)
         {
-            byte[] result;
             byte[] data = Convert.FromBase64String(encryptPassword);
             using (MD5CryptoServiceProvider md5=new MD5CryptoServiceProvider())
             {
                 byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hashes));
                 using (TripleDESCryptoServiceProvider tripleDes=new TripleDESCryptoServiceProvider(){Key = keys,Mode = CipherMode.ECB,Padding = PaddingMode.PKCS7})
                 {
-                    ICryptoTransform cryptoTransform = tripleDes.CreateEncryptor();
-                    result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    ICryptoTransform cryptoTransform = tripleDes.CreateDecryptor();
+                   byte[] result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                   return UTF8Encoding.UTF8.GetString(result);
                 }
             }
 
-            return UTF8Encoding.UTF8.GetString(result);
+            return "";
         }
         
         public string Encrypt(string password, string hash)
         {
-            byte[] result;
             byte[] data = UTF8Encoding.UTF8.GetBytes(password);
             using (MD5CryptoServiceProvider md5=new MD5CryptoServiceProvider())
             {
@@ -51,28 +50,29 @@ namespace ShakikulFramework.Method
                 using (TripleDESCryptoServiceProvider tripleDes=new TripleDESCryptoServiceProvider(){Key = keys,Mode = CipherMode.ECB,Padding = PaddingMode.PKCS7})
                 {
                     ICryptoTransform cryptoTransform = tripleDes.CreateEncryptor();
-                    result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    byte[] result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(result, 0, result.Length);
                 }
             }
 
-            return Convert.ToBase64String(result, 0, result.Length);
+            return "";
         }
         
         public string Decrypt(string encryptPassword, string hash)
         {
-            byte[] result;
             byte[] data = Convert.FromBase64String(encryptPassword);
             using (MD5CryptoServiceProvider md5=new MD5CryptoServiceProvider())
             {
                 byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
                 using (TripleDESCryptoServiceProvider tripleDes=new TripleDESCryptoServiceProvider(){Key = keys,Mode = CipherMode.ECB,Padding = PaddingMode.PKCS7})
                 {
-                    ICryptoTransform cryptoTransform = tripleDes.CreateEncryptor();
-                    result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    ICryptoTransform cryptoTransform = tripleDes.CreateDecryptor();
+                    byte[] result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+                    return UTF8Encoding.UTF8.GetString(result);
                 }
             }
 
-            return UTF8Encoding.UTF8.GetString(result);
+            return "";
         }
     }
 }
